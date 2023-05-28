2b70210986bce4d189aa83cae35c72ae66109387
castle3B29 <59777589+castle3B29@users.noreply.github.com>
2023-05-28 12:21:30 -0400

    Update string1.c

--- string1.c
+++ string1.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 char * test_string = "hello world";
 
+char test_me_global[] = "beefdeadcodedead";
+
 int main(){
 
     int i = 0;
@@ -9,6 +11,7 @@ int main(){
     int stop_2 = 6;
     char test_me[8] = "abcdefg";
     char test_me2[] = "deadbeefdeadcode";
+    ///testing test_me to see if we try to alter by putting string literal at positions 5 and 6
     while(i < 8){
         if(i == stop){
             test_me[i] = "w";
@@ -20,6 +23,7 @@ int main(){
     }
     printf("%s \n", test_me);
     i = 0;
+    ///testing test_me2 to see if we put in a string literal at positiong 6
     while (i < 17){
         if (i == stop_2){
             test_me2[i] = "X";
@@ -28,4 +32,23 @@ int main(){
 
     }
     printf("%s \n", test_me2);
+
+    //testing global variable
+    i = 0;
+    while (i < 17){
+        if (i == stop_2){
+            test_me_global[i] = 'X';
+        }
+        i++;
+    }
+    printf("%s \n", test_me_global);
+    i = 0;
+    while (i < 17){
+        if (i == stop_2){
+            test_me_global[i] = "X";
+        }
+        i++;
+    }
+    printf("%s \n", test_me_global);
+
 }
